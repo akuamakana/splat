8f0a5ccf881c03131a6078e8f0ef3e480c2dbbf9
akuamakana <akuamakana@gmail.com>
2021-11-02 14:10:13 -0700

    Delete server/dist directory

--- server/dist/controllers/hello.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = void 0;
-const hello = (response) => {
-    response.status(200).send('Hello world');
-};
-exports.hello = hello;
-//# sourceMappingURL=hello.js.map
--- server/dist/routes/hello.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloRoute = void 0;
-const hello_1 = require("../controllers/hello");
-const helloRoute = (app) => {
-    app.get('/hello', hello_1.hello);
-};
-exports.helloRoute = helloRoute;
-//# sourceMappingURL=hello.js.map
